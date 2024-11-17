3f85f57e65ce524fb4a5c87b905a5aca8c93632c
Matheus Marques <90071675+marques-matheus@users.noreply.github.com>
2024-11-17 16:44:44 -0300

    feat: layout footer

--- src/app/organisms/footer/Footer.tsx
+++ src/app/organisms/footer/Footer.tsx
@@ -1,33 +1,65 @@
 import Section from "@/app/templates/section/Section";
 import Image from "next/image";
+import { BiChevronRight } from "react-icons/bi";
+import { BsLinkedin, BsInstagram, BsFacebook } from "react-icons/bs";
+
+const links = [
+    {
+        href: "#home",
+        label: "Ínicio",
+
+    },
+    {
+        href: "#about",
+        label: "Sobre",
+    },
+    {
+        href: "#services",
+        label: "Serviços",
+    },
+    {
+        href: "#contact",
+        label: "Contato",
+    },
+]
 
 const Footer = () => {
     return (
         <Section id="footer" bg="bg-marks-700" classes="flex flex-col gap-4">
             <div className="flex flex-col md:flex-row items-center justify-between">
                 <article className="text-gray-200 flex flex-col space-y-1">
-                    <Image src="/logo-white.svg" alt="Logo" width={100} height={100} className="w-36 lg:w-48" />
+                    <Image src="/logo-white.svg" alt="Logo" width={100} height={100} className="w-36 " />
                     <span>(21) 99999-9999</span>
                     <span>4o4fX@example.com</span>
                 </article>
-                <article>
-                    <h3 className="text-marks-600 text-xl font-bold">Serviços</h3>
+                <article className="flex-col items-start space-y-2 hidden md:flex">
+                    <h3 className="text-white text-xl font-bold">Links Úteis</h3>
                     <div>
                         <ul>
-                            <li>Consultoria</li>
-                            <li>Desenvolvimento</li>
-                            <li>Outros</li>
+                            {links.map((link) => (
+                                <li key={link.href} className="text-gray-200 flex items-center gap-2 w-24  hover:border-r-4 hover:border-marks-100 hover:text-marks-100">
+                                    <BiChevronRight size={20} />
+                                    <a href={link.href}>{link.label}</a>
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </article>
-                <div className="flex flex-col gap-4">
-                    <h3 className="text-marks-600 text-xl font-bold">Contato</h3>
-                    <div className="flex gap-4">
-                        <Image src="/whatsapp-icon.png" alt="Logo" width={40} height={40} className="w-12" />
-                        <Image src="/instagram-icon.png" alt="Logo" width={40} height={40} className="w-12" />
-                        <Image src="/linkedin-icon.png" alt="Logo" width={40} height={40} className="w-12" />
+                <article className="flex flex-col gap-4 max-w-sm">
+                    <h3 className="text-white text-xl font-bold">Contato</h3>
+                    <p className="text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vel a, officiis nostrum recusandae.</p>
+                    <div className="flex gap-4 ">
+                        <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                            <BsLinkedin size={28} className="hover:text-marks-100 text-gray-200" />
+                        </a>
+                        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                            <BsInstagram size={28} className="hover:text-marks-100 text-gray-200" />
+                        </a>
+                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                            <BsFacebook size={28} className="hover:text-marks-100 text-gray-200" />
+                        </a>
                     </div>
-                </div>
+                </article>
             </div>
         </Section>
     );
