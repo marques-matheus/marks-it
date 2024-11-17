1a0c81659cca1fb8653204c941eb153d101f87da
Matheus Marques <90071675+marques-matheus@users.noreply.github.com>
2024-11-17 15:24:32 -0300

    ajustes no header

--- src/app/organisms/header/Header.tsx
+++ src/app/organisms/header/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Header: React.FC = () => {
     return (
         <header className="bg-marks-600 py-4">
-            <div className="container mx-auto flex items-center px-5 justify-between md:m-auto md:px-20">
+            <div className="container mx-auto flex items-center px-5 justify-between lg:px-20 xl:px-16">
                 <Image src="/logo-white.svg" alt="Logo" width={100} height={100} className="w-36 lg:w-48" />
                 <NavList />
             </div>
