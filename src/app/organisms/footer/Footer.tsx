98a99bc91d9aeeb72bcf18f78a93726b84e413e8
Matheus Marques <90071675+marques-matheus@users.noreply.github.com>
2024-11-17 17:20:26 -0300

    ajustes

--- src/app/organisms/footer/Footer.tsx
+++ src/app/organisms/footer/Footer.tsx
@@ -1,7 +1,8 @@
 import Section from "@/app/templates/section/Section";
 import Image from "next/image";
 import { BiChevronRight } from "react-icons/bi";
-import { BsLinkedin, BsInstagram, BsFacebook } from "react-icons/bs";
+import { BsLinkedin, BsInstagram, BsTwitter, BsTelephone } from "react-icons/bs";
+import { FiMail } from "react-icons/fi";
 
 const links = [
     {
@@ -25,12 +26,14 @@ const links = [
 
 const Footer = () => {
     return (
-        <Section id="footer" bg="bg-marks-700" classes="flex flex-col gap-4">
-            <div className="flex flex-col md:flex-row items-center justify-between">
-                <article className="text-gray-200 flex flex-col space-y-1">
-                    <Image src="/logo-white.svg" alt="Logo" width={100} height={100} className="w-36 " />
-                    <span>(21) 99999-9999</span>
-                    <span>4o4fX@example.com</span>
+        <Section id="footer" bg="bg-gradient-to-b from-marks-800 to-marks-600" classes="flex flex-col gap-4 xl:py-0 xl:pt-16">
+            <div className="flex flex-col md:flex-row justify-between items-center md:items-start">
+                <article className="text-gray-200 flex flex-col items-center">
+                    <Image src="/logo-white.svg" alt="Logo" width={100} height={100} className="w-36 mb-5" />
+                    <div>
+                        <span className="flex items-center gap-2"><BsTelephone size={20} />(21) 97962-1864</span>
+                        <span className="flex items-center gap-2"><FiMail size={20} />contato@marksit.info</span>
+                    </div>
                 </article>
                 <article className="flex-col items-start space-y-2 hidden md:flex">
                     <h3 className="text-white text-xl font-bold">Links Úteis</h3>
@@ -45,22 +48,23 @@ const Footer = () => {
                         </ul>
                     </div>
                 </article>
-                <article className="flex flex-col gap-4 max-w-sm">
-                    <h3 className="text-white text-xl font-bold">Contato</h3>
-                    <p className="text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vel a, officiis nostrum recusandae.</p>
+                <article className="flex flex-col gap-4 max-w-56 lg:max-w-sm items-center self-certer my-5 md:my-0">
+                    <h3 className="text-white text-xl font-bold hidden md:block">Contato</h3>
+                    <p className="text-gray-200 hidden md:block">Entre em contato conosco</p>
                     <div className="flex gap-4 ">
-                        <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.linkedin.com/in/marks-it-consulting-e-services/" target="_blank" rel="noopener noreferrer">
                             <BsLinkedin size={28} className="hover:text-marks-100 text-gray-200" />
                         </a>
-                        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.instagram.com/marks.it/" target="_blank" rel="noopener noreferrer">
                             <BsInstagram size={28} className="hover:text-marks-100 text-gray-200" />
                         </a>
-                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                            <BsFacebook size={28} className="hover:text-marks-100 text-gray-200" />
+                        <a href="https://x.com/it_marks" target="_blank" rel="noopener noreferrer">
+                            <BsTwitter size={28} className="hover:text-marks-100 text-gray-200" />
                         </a>
                     </div>
                 </article>
             </div>
+            <span className="text-gray-200 text-center text-sm mt-10">&copy; {new Date().getFullYear()} Marks IT Consulting e Services. Todos os direitos reservados.</span>
         </Section>
     );
 };
--- src/app/organisms/home/home.tsx
+++ src/app/organisms/home/home.tsx
@@ -8,7 +8,7 @@ import Section from "@/app/templates/section/Section";
 const Home = () => {
     return (
         <Section
-            classes={`h-fit text-center flex items-center justify-around flex-col md:flex-row bg-gradient-to-b from-marks-600 to-marks-100`}
+            classes={`h-fit text-center flex items-center justify-around flex-col md:flex-row bg-gradient-to-b from-marks-600 to-marks-800`}
             id="home">
             <article className="text-start max-w-xl">
                 <h1 className="text-3xl font-bold text-white">
@@ -19,7 +19,7 @@ const Home = () => {
                     Quasi repellat, eaque repellendus aperiam tempore, quidem,
                 </p>
             </article>
-            <Image src="/hero-img.png" alt="animação com duas pessoas na frente analizando dados, ao fundo um foguete decolando e alguns dados de BI" className="w-auto md:w-2/4 lg:w-96 xl:w-2/4" width={600} height={400} />
+            <Image src="/tech-company-animate.svg" alt="animação com duas pessoas na frente analizando dados, ao fundo um foguete decolando e alguns dados de BI" className="w-auto md:w-2/4 lg:w-96 xl:w-2/4" width={600} height={400} />
             <a className="hidden" href="https://storyset.com/business">Business illustrations by Storyset</a>
         </Section>
     );
